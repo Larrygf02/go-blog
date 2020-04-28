7c0cf1764e9a5566d5b808d31c4a0bccc85ad431
larrygf02 <raulgf_02@hotmail.com>
2020-04-28 16:33:16 -0500

    Conexion con postgres y golang

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
 
 func main() {
 	fmt.Println("Hello world")
+	InitialMigration()
+}
+
+var db *gorm.DB
+var err error
+
+func InitialMigration() {
+	db, err := gorm.Open("postgres", "host=localhost sslmode=disable port=5433 user=postgres dbname=bloggo password=123")
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Failed to connect to database")
+	}
+	fmt.Println("You are connect")
+	defer db.Close()
+	db.AutoMigrate()
 }
