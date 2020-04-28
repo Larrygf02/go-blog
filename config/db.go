3338b926f04d83331843c43c83ea51f0c3652f52
larrygf02 <raulgf_02@hotmail.com>
2020-04-28 16:57:26 -0500

    Config db in new package

---
+++ config/db.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+var db *gorm.DB
+var err error
+
+func InitialMigration() {
+	db, err := gorm.Open("postgres", "host=localhost sslmode=disable port=5433 user=postgres dbname=bloggo password=123")
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Failed to connect to database")
+	}
+	fmt.Println("You are connect")
+	defer db.Close()
+	db.AutoMigrate()
+}
--- main.go
+++ main.go
@@ -3,25 +3,10 @@ package main
 import (
 	"fmt"
 
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/larrygf02/go-blog/config"
 )
 
 func main() {
 	fmt.Println("Hello world")
-	InitialMigration()
-}
-
-var db *gorm.DB
-var err error
-
-func InitialMigration() {
-	db, err := gorm.Open("postgres", "host=localhost sslmode=disable port=5433 user=postgres dbname=bloggo password=123")
-	if err != nil {
-		fmt.Println(err.Error())
-		panic("Failed to connect to database")
-	}
-	fmt.Println("You are connect")
-	defer db.Close()
-	db.AutoMigrate()
+	config.InitialMigration()
 }
