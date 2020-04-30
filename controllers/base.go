af9817ab6f3694f87f4e98c91d8d3c2786ec3a2c
larrygf02 <raulgf_02@hotmail.com>
2020-04-30 17:05:35 -0500

    Improve: Mejor estructura de los archivos en golang

---
+++ controllers/base.go
@@ -0,0 +1,28 @@
+package controllers
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	"github.com/larrygf02/go-blog/views"
+)
+
+type Server struct {
+	DB     *gorm.DB
+	Router *mux.Router
+}
+
+func (s *Server) Initialize(DBDriver, DBUser, DBPassword, DBPort, DBHost, DBName string) {
+	var err error
+	// Conectando con base de datos POSTGRES
+	DBURL := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable", DBHost, DBPort, DBUser, DBName, DBPassword)
+	fmt.Println(DBURL)
+	s.DB, err = gorm.Open(DBDriver, DBURL)
+	if err != nil {
+		log.Fatal("This is the error:", err)
+	}
+	s.DB.Debug().AutoMigrate(&views.User{}, &views.Storie{}, &views.StorieVisit{})
+	s.Router = mux.NewRouter()
+}
