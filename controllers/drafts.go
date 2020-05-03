3840bd46f483c9a3ae7eded05ea37440d707b52e
larrygf02 <raulgf_02@hotmail.com>
2020-05-03 09:52:43 -0500

    Draft Updated Endpoint

--- controllers/drafts.go
+++ controllers/drafts.go
@@ -2,8 +2,11 @@ package controllers
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/larrygf02/go-blog/models"
 	send_response "github.com/larrygf02/go-blog/response"
 )
@@ -21,4 +24,27 @@ func (s *Server) NewDraft(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	send_response.JSON(w, http.StatusCreated, created)
+}
+
+func (s *Server) UpdateDraft(w http.ResponseWriter, r *http.Request) {
+	s.DB.LogMode(true)
+	parameters := mux.Vars(r)
+	id, err := strconv.Atoi(parameters["id"])
+	if err != nil {
+		send_response.ERROR(w, http.StatusInternalServerError, err)
+		return
+	}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		send_response.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	var draft models.Draft
+	json.Unmarshal(body, &draft)
+	draft.ID = id
+	updated, err := draft.Update(s.DB)
+	if err != nil {
+		send_response.ERROR(w, http.StatusInternalServerError, err)
+	}
+	send_response.JSON(w, http.StatusOK, updated)
 }
--- controllers/routes.go
+++ controllers/routes.go
@@ -15,6 +15,7 @@ func (s *Server) InitializeRoutes() {
 	s.Router.HandleFunc("/storieapplause", s.SaveStorieApplause).Methods("POST")
 	s.Router.HandleFunc("/storiecomment", s.SaveStorieComment).Methods("POST")
 	s.Router.HandleFunc("/storiecomment/{id}", s.UpdateStorieComment).Methods("PUT")
+	s.Router.HandleFunc("/draft", s.NewDraft).Methods("POST")
 	// Habilitar CORS
 	s.Router.Use(mux.CORSMethodMiddleware(s.Router))
 	s.Router.Use(middlewares.SetMiddlewareJSON)
