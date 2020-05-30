fa0b10afe9d50fab6ec2e49cf74411deeaae124f
larrygf02 <raulgf_02@hotmail.com>
2020-05-30 10:47:26 -0500

    Add middleware token

--- middlewares/middlewares.go
+++ middlewares/middlewares.go
@@ -1,6 +1,9 @@
 package middlewares
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func SetMiddlewareJSON(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -8,5 +11,13 @@ func SetMiddlewareJSON(next http.Handler) http.Handler {
 		w.Header().Set("Content-Type", "appliction/json")
 		//w.Header().Set("Access-Control-Allow-Origin", "*")
 		next.ServeHTTP(w, r)
+	})
+}
+
+func TokenMiddlewareJSON(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		m := r.Method
+		log.Println(m)
+		log.Println(r.RemoteAddr)
 	})
 }
